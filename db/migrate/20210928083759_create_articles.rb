773efb152316f238c68282e1805cb69429077303
Christopher Astfalk <christopher.astfalk@icloud.com>
2021-09-28 10:42:10 +0200

    add article scaffold

--- db/migrate/20210928083759_create_articles.rb
+++ db/migrate/20210928083759_create_articles.rb
@@ -1,9 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
-      t.string       :title
-      t.text         :body
-      t.datetime     :published_at
+      t.string :title
+      t.text :body
+      t.datetime :published_at
 
       t.timestamps
     end
