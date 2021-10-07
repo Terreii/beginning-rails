92a1cd3c50e65588c7b29d7e2edc906896707c5a
Christopher Astfalk <christopher.astfalk@icloud.com>
2021-10-08 01:21:45 +0200

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create email: 'mary@example', password: 'guessit'
+
+Category.create [
+  { name: 'Programming' },
+  { name: 'Event' },
+  { name: 'Travel' },
+  { name: 'Music' },
+  { name: 'TV' }
+]
+
