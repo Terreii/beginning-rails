2a2977a5d13735a05eeea68ea70ec39dcba69948
Christopher Astfalk <christopher.astfalk@icloud.com>
2021-10-26 00:29:35 +0200

    add a cover image to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,10 @@ class Article < ApplicationRecord
   has_and_belongs_to_many :categories
   has_many :comments
 
+  has_one_attached :cover_image
+  attr_accessor :remove_cover_image
+  after_save { cover_image.purge if remove_cover_image == '1' }
+
   scope :published, -> { where.not(published_at: nil) }
   scope :draft, -> { where(published_at: nil) }
   scope :recent, -> { where('articles.published_at > ?', 1.week.ago.to_date) }
