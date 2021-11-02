a420d1cd3c17fe2bf7ff461b885249dc01517cab
Christopher Astfalk <christopher.astfalk@icloud.com>
2021-11-02 01:10:57 +0100

    add an answer mail to draft articles

--- test/mailers/previews/draft_articles_mailer_preview.rb
+++ test/mailers/previews/draft_articles_mailer_preview.rb
@@ -1,5 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/draft_articles_mailer
 class DraftArticlesMailerPreview < ActionMailer::Preview
+  # Preview this email at http://localhost:3000/rails/mailers/draft_articles_mailer/created
+  def created
+    DraftArticlesMailer.created('test@example.com', Article.first)
+  end
 
   # Preview this email at http://localhost:3000/rails/mailers/draft_articles_mailer/no_author
   def no_author
