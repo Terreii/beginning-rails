e8c03b8b8857e85d7d42a1e0363e51eaeeafe628
Christopher Astfalk <christopher.astfalk@icloud.com>
2021-11-03 02:52:37 +0100

    add a EmailAFriend model

---
+++ app/models/email_a_friend.rb
@@ -0,0 +1,8 @@
+class EmailAFriend
+  inlcode ActiveModel::Model
+
+  attr_accessor :name, :email
+
+  validates :name, :email, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+end
