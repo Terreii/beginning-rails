8ad78d5a4e8853ea63af4558c942f56871b648e1
Christopher Astfalk <christopher.astfalk@icloud.com>
2021-11-08 20:46:06 +0100

    add tests for article crud happy paths

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create article" do
+    article = Article.new
+
+    article.user = users(:eugene)
+    article.title = 'Test Article'
+    article.body = 'Test body'
+
+    assert article.save
+  end
+
+  test "should find article" do
+    article_id = articles(:welcome_to_rails).id
+    assert_nothing_raised { Article.find(article_id) }
+  end
+
+  test "should update article" do
+    article = articles(:welcome_to_rails)
+    article.update(title: 'New title')
+    assert_equal 'New title', article.reload.title
+  end
+
+  test "should destroy article" do
+    article = articles(:welcome_to_rails)
+    article.destroy
+    assert_raise(ActiveRecord::RecordNotFound) { Article.find(article.id) }
+  end
 end
