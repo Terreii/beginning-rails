896b447536e6cda0991fffdc2fe40f23bf70ea4a
Christopher Astfalk <christopher.astfalk@icloud.com>
2021-11-11 00:51:12 +0100

    add more articles controller tests

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -36,10 +36,24 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should raise RecordNotFound when non-owner tries to get edit" do
+    login_as :lauren
+    assert_raises(ActiveRecord::RecordNotFound) do
+      get edit_article_url(@article)
+    end
+  end
+
   test "should update article" do
     login_as :eugene
     patch article_url(@article), params: { article: { body: @article.body, excerpt: @article.excerpt, location: @article.location, published_at: @article.published_at, title: @article.title } }
     assert_redirected_to article_url(@article)
+  end
+
+  test "should raise RecordNotFound when non-owner tries to update article" do
+    login_as :lauren
+    assert_raises(ActiveRecord::RecordNotFound) do
+      patch article_url(@article), params: { article: { body: @article.body, excerpt: @article.excerpt, location: @article.location, published_at: @article.published_at, title: @article.title } }
+    end
   end
 
   test "should destroy article" do
@@ -49,5 +63,26 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to articles_url
+  end
+
+  test "should raise RecordNotFound when non-owner tries to destroy article" do
+    login_as :lauren
+    assert_raises(ActiveRecord::RecordNotFound) do
+      delete article_url(@article)
+    end
+  end
+
+  test "should redirect to article url when submitting valid email a friend form" do
+    post notify_friend_article_url(@article), params: {
+      email_a_friend: { name: 'Joe', email: 'joe@example.com' }
+    }, xhr: true
+    assert_redirected_to article_url(@article)
+  end
+
+  test "should respond with unprocessable_entity when submitting invalid email a friend form" do
+    post notify_friend_article_url(@article), params: {
+      email_a_friend: { name: 'Joe', email: 'notAnEmail' }
+    }, xhr: true
+    assert_response :unprocessable_entity
   end
 end
